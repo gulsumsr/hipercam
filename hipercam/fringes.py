3b82f16f2d90295941259ef03caeb60f3372010f
Tom Marsh <t.r.marsh@warwick.ac.uk>
2018-11-21 21:46:39 +0000

    updates to new fringing classes
    Rectangle added in along with serialisation methods

--- hipercam/fringes.py
+++ hipercam/fringes.py
@@ -108,6 +108,77 @@ class Circle(Fregion):
             self.xcen, self.ycen, self.radius, self.add
         )
 
+class Rectangle(Fregion):
+
+    """Defines a rectangular region for measuring fringes
+
+    """
+
+    def __init__(self, x1, x2, y1, y2, add):
+        """
+        Arguments::
+
+           x1 : float
+              Left-hand X-ordinate of rectangle, starting from
+              1 at extreme left of CCD, unbinned pixels.
+
+           x2 : float
+              Right-hand X-ordinate of rectangle, starting from
+              1 at extreme left of CCD, unbinned pixels.
+
+           y1 : float
+              Lower Y-ordinate of rectangle, starting from
+              1 at bottom of CCD, unbinned pixels.
+
+           y2 : float
+              Upper Y-ordinate of rectangle, starting from
+              1 at bottom of CCD, unbinned pixels.
+
+           add : bool
+              True to add pixels, False to remove.
+        """
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        self.add = add
+
+    def apply(self, x, y, mask=None):
+        """Computes the pixels to be used for estimating the fringe
+        amplitude.
+
+        Arguments::
+
+           x : 2D array
+              X-ordinates of pixels
+
+           y : 2D array
+              Y-ordinates of pixels
+
+           mask : None | 2D array
+              if specified as an input, this will be modified
+              according to the Fregion
+
+        Returns: 2D logical array showing which pixels are used.
+        """
+        inside = (x>=self.x1) & (x<=self.x2) & (y>=self.y1) & (y<=self.y2)
+        if mask is None:
+            if self.add:
+                mask = inside
+            else:
+                mask = ~inside
+        else:
+            if self.add:
+                mask |= inside
+            else:
+                mask &= ~inside
+        return mask
+
+    def __repr__(self):
+        return 'Rectangle(x1={!r}, x2={!r}, y1={!r}, y2={!r}, add={!r})'.format(
+            self.x1, self.x2, self.y1, self.y2, self.add
+        )
+
 #    def copy(self, memo=None):
 #        """Returns with a copy of the Aperture"""
 #        return Aperture(
@@ -138,19 +209,26 @@ class _Encoder(json.JSONEncoder):
 
     def default(self, obj):
 
-        if isinstance(obj, Aperture):
+        if isinstance(obj, Circle):
             return OrderedDict(
                 (
-                    ('Comment', 'hipercam.Aperture'),
+                    ('Comment', 'hipercam.fringes.Circle'),
                     ('x', obj.x),
                     ('y', obj.y),
-                    ('rtarg', obj.rtarg),
-                    ('rsky1', obj.rsky1),
-                    ('rsky2', obj.rsky2),
-                    ('ref', obj.ref),
-                    ('mask', obj.mask),
-                    ('extra', obj.extra),
-                    ('link', obj.link),
+                    ('radius', obj.radius),
+                    ('add', obj.add),
+                    )
+                )
+
+        elif isinstance(obj, Rectangle):
+            return OrderedDict(
+                (
+                    ('Comment', 'hipercam.fringes.Rectangle'),
+                    ('x1', obj.x1),
+                    ('x2', obj.x2),
+                    ('y1', obj.y1),
+                    ('y2', obj.y2),
+                    ('add', obj.add),
                     )
                 )
 
@@ -163,15 +241,38 @@ class _Decoder(json.JSONDecoder):
 
     def object_hook(self, obj):
         # looks out for Aperture objects. Everything else done by default
-        if 'rtarg' in obj and 'rsky1' in obj and 'rsky2' in obj and 'link' in obj:
-            return Aperture(
-                obj['x'], obj['y'], obj['rtarg'], obj['rsky1'], obj['rsky2'],
-                obj['ref'], obj['mask'], obj['extra'], obj['link']
-            )
+        if 'Comment' in obj:
+            if obj['Comment'] == 'hipercam.fringes.Circle':
+                return Circle(
+                    obj['x'], obj['y'], obj['radius'], obj['add']
+                )
+
+            elif obj['Comment'] == 'hipercam.fringes.Rectangle':
+                return Rectangle(
+                    obj['x1'], obj['x2'], obj['y1'], obj['y2'], obj['add']
+                )
 
         return obj
 
 if __name__ == '__main__':
 
-    c = Circle(200, 250, 10, True)
-    print(c)
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    x = np.linspace(1,500,500)
+    y = np.linspace(1,500,500)
+    X,Y = np.meshgrid(x,y)
+
+    l = []
+    l.append(Rectangle(20, 400, 31, 300, True))
+    l.append(Circle(200, 250, 10, False))
+    l.append(Circle(260, 150, 20, False))
+    l.append(Rectangle(200, 450, 310, 400, True))
+    mask = None
+    for fr in l:
+        mask = fr.apply(X,Y,mask)
+
+    extent=(x.min()-0.5,x.max()+0.5,y.min()-0.5,y.max()+0.5)
+    plt.imshow(mask,origin='lower',extent=extent)
+    plt.show()
+
